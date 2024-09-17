bb42af9fe6e302cc2d6acc66eea0923a37723c3d
Aryan-Fartash <fartasharyan@gmail.com>
2024-09-17 14:55:13 -0600

    copying home and blog files
    copying home and blog files

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,include
+from blog.views import blog_view
+
+app_name='blog'
+
+urlpatterns = [
+    path('', blog_view , name='index' ),
+    path('4-challenges-international-students-face-in-the-us/')
+]
---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def blog_view(request):
+    return render(request,'blog/index.html')
+
